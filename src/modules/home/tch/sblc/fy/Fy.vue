d77c19c8d37f54697d09f8df09e35a82609f11c3
Gao, Pan <gpchinese@126.com>
2018-09-14 13:18:55 +0800

    update

--- src/modules/home/tch/sblc/fy/Fy.vue
+++ src/modules/home/tch/sblc/fy/Fy.vue
@@ -6,7 +6,7 @@
           <div class="col-md-12">
             <div class="section-title-wrapper">
               <div class="section-title">
-                <h3>2018/2019 学年费用一览</h3>
+                <h3>2018/2019 學年費用一覽</h3>
               </div>
             </div>
           </div>
@@ -16,47 +16,33 @@
             <div class="about-text-container">
               <table>
                 <thead>
-                  <th>年级</th>
-                  <th>学费（港币）</th>
-                  <th>捐赠（港币）</th>
-                  <th>合計</th>
+                  <th>年級</th>
+                  <th>收費（港幣）</th>
                 </thead>
                 <tbody>
                   <tr>
                     <td>8</td>
                     <td>148,000</td>
-                    <td>60,000</td>
-                    <td>208,000</td>
                   </tr>
                   <tr>
                     <td>9</td>
                     <td>148,000</td>
-                    <td>60,000</td>
-                    <td>208,000</td>
                   </tr>
                   <tr>
                     <td>10</td>
                     <td>148,000</td>
-                    <td>60,000</td>
-                    <td>208,000</td>
                   </tr>
                   <tr>
                     <td>11</td>
                     <td>148,000</td>
-                    <td>60,000</td>
-                    <td>208,000</td>
                   </tr>
                   <tr>
                     <td>12</td>
                     <td>168,000</td>
-                    <td>100,000</td>
-                    <td>268,000</td>
                   </tr>
                   <tr>
                     <td>13</td>
                     <td>168,000</td>
-                    <td>100,000</td>
-                    <td>268,000</td>
                   </tr>
                 </tbody>
               </table>
@@ -80,8 +66,8 @@
           <div class="col-md-offset-1 col-md-10">
             <div class="about-text-container">
               <ul>
-                <li><i class="fa fa-circle"></i> 確認接受學位通知 - 一次性付款300港幣</li>
-                <li><i class="fa fa-circle"></i> 學生保險 - 這是必要的，但學生如有其他保險覆蓋就可以免除。 請注意，學院不提供保險計劃來支付學費退款，我們鼓勵家長自行安排。</li>
+                <li><i class="fa fa-circle"></i> 確認接受學位通知 - 一次性付款300港蔽</li>
+                <li><i class="fa fa-circle"></i> 學生保險 - 這是必要的，但學生如有其他保險覆蓋就可以免除。 請註意，學院不提供保險計劃來支付學費退款，我們鼓勵家長自行安排。</li>
                 <li><i class="fa fa-circle"></i> 雅思考試（如有需要）</li>
                 <li><i class="fa fa-circle"></i> 公開考試費用*</li>
                 <li><i class="fa fa-circle"></i> 額外的一對一教學學費（A-level，IELTS和音樂課程每小時額外收費為600港元）</li>
