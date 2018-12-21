2f2288727278468dc2badf19aca5e018b1a3129e
Chen <hong-yu.chen@dxc.com>
2018-12-21 10:32:27 +0800

    add images

---
+++ src/api/data/czzx/data.js
@@ -0,0 +1,93 @@
+/*1.1創校之路
+
+崇正中學為乔港崇正總會屬下之中學；
+Tsung Tsin Middle School is a middle school subordinated to 
+tho Hong Kong Tsung Tsin Association;
+
+■ 1957年起全心盡意施教勸學：
+A school which has been wholeheartedly educating and 
+teaching students since 1957;
+
+■乃全港唯一不問出身、不计宗教背景、不看“死文憑”， 
+唯重梘學子“自我完善”、“自我超脫”之研學精神的中势 
+The only school in Hong Kong that only values students
+"self-improvement and "self-detachment" spirits instead 
+on rigidly emphasize on family birth, religious background, or 
+"stiff diploma";
+
+■—所真正關懷學生“現在與將來”的中學。
+A school that truly cares for the students* present and future.
+
+1.2歷史佳績
+
+■香港崇正總會是目前全世界創辦時間最早，影響力最大 
+的客家人社會组織之一,成立於1921年9月29日，至 
+今已經走過了風風雨雨的90年歲月。
+ 
+Hong Kong Tsung Tsin Association is one of the world' s 
+earliest and most influential Hakka social organizations.It was 
+established on September 29,1921 and has been through the 
+ups and downs of 90 years.
+
+■香港崇正總會，原名“旅港崇正工商總會"，創建人有 
+張發奎、胡文虎、林翼中、賴際熙、李瑞琴、黄茂林等。 
+The founders of Hong Kong Tsung Tsin Association, formerly 
+known as "Tsung Tsin Industry and Commerce Association in 
+Hong Kong" include Zhang Fakui, Aw Boon Haw, Lin Yizhong, 
+Lai Jixi, Li Ruiqin, Huang Maolin and so on.
+
+■ 1926年 徐 仁 苺 先 生 建 饑 ，總會冠 以 “工商”二字範圍太 
+狭窄，不利於團結廣大客家鄉親，遂決議稱為“香港崇 
+正總會”。
+In 1926.Mr.Xu Renshou suggested that the scope was limited 
+by the "Commerce and Induslry" in the name, which was not 
+conducive to uniting Hakka folks, so the name was amended 
+as "Hong Kong Tsung Tsin Association" since then.
+
+■香港崇正總會在1922年至1940年期間，先後創辦了4 
+間崇正義學，其經费概由會長胡文虎先生讚助。
+During the period from 1922 to 1940, the Hong Kong Tsung 
+Tsin Association established four Tsung Tsin charity schools, 
+and the funds were sponsored by the president Mr.Aw Boon 
+Haw.
+■ 總會先 後 創 辦 了《崇正月刊》、《崇正會刊》, 還出版 
+过共15卷的《客族考源》一书。
+The Association has successively launched the Tsung Tsin 
+Monthly and Tsung Tsin Association Journal, and published 
+the book total 5 volumes of Hakka Origin Investigation.
+
+■更多歷史成就，请参閱官方網站http://www.ttms.edu.hk 
+For more achievements over the past, please refer to the 
+official website http://www.ttms.edu.hk
+
+1.3崇正文化
+TTMS Cufture
+■重視學子“全球化”與“本地化”的融合發展。
+Emphasis on the integration of Mglobalizationn and 
+“localization” of students.
+
+■ 重点培养學子”獨立學晋能力”及”自主學習能力”的素质
+Focus on cultivating sludents' "independent learning ability" 
+and 'self-learning ability*.
+■融合西方教育的科學性與中國傳統文化的民族性學習儍勢。
+
+Integrate the scientific nature of western education and the 
+national advantage of Chinese cultural heritage.
+
+
+
+張紫伶博士 /Dr.Sammi Yu
+“自信，專注，獨立，熱誠"
+
+-我們相倌學生們應該在不同議題上秉持獨立的見解，不論是涉及 
+全球或民族層面的议題，亦或是涉及當地或個人層面的事項。為了 
+幫助學生擁有兼容東西方文化的價值觀，我們專注於培養他們的自 
+信心、專注力、獨立性和熱誠。
+"confident Concentration Independent Passion"
+We believe all the student should have their own perspectives on different 
+issues globally, nationally, locally and personally.In order to help student 
+acquires both Eastern and Western culture values, we focus on developing 
+their confidence, concentration, independence and passion characteristics.
+
+
+*/
--- src/modules/home/czzx/establishment/Establishment.vue
+++ src/modules/home/czzx/establishment/Establishment.vue
@@ -1,5 +1,33 @@
 <template>
-  <main class="content-main" role="main" v-html="content">
+  <!-- <main class="content-main" role="main" v-html="content"> -->
+  <main class="content-main establishment" role="main">
+  	<!-- <div class="school-name">
+  		<p class="name-title">香港崇正中學</p>
+  		<p class="bold-line"></p>
+  		<p>
+  			<img :src="imgUrl('gate.png')">
+  		</p>
+  		
+  	</div> -->
+  	<div class="establishment-list">
+  		<ul class="info-list">
+  			<li>■  崇正中學為乔港崇正總會屬下之中學；</li>
+
+				<li>■  1957年起全心盡意施教勸學：</li>
+
+				<li>■  乃全港唯一不問出身、不计宗教背景、不看“死文憑”，唯重梘學子“自我完善”、“自我超脫”之研學精神的中势</li>
+
+				<li>■  —所真正關懷學生“現在與將來”的中學。</li>
+
+				<li>■  1926年徐仁苺先生建饑，總會冠以“工商”二字範圍太狭窄，不利於團結廣大客家鄉親，遂決議稱為“香港崇正總會”。</li>
+
+				<li>■  香港崇正總會在1922年至1940年期間，先後創辦了4間崇正義學，其經费概由會長胡文虎先生讚助。</li>
+
+				<li>■  總會先後創辦了《崇正月刊》、《崇正會刊》,還出版过共15卷的《客族考源》一书。</li>
+
+				<li>■  更多歷史成就，请参閱官方網站<a href="http://www.ttms.edu.hk" target="">http://www.ttms.edu.hk</a></li>
+  		</ul>
+  	</div>
   </main>
 </template>
 <script src="./establishment.js"></script>
