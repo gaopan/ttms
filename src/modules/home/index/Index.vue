75c0ecbbccd90ed7f8cf2784cf79a763cadc2c51
Gao, Pan <gpchinese@126.com>
2019-01-07 15:50:40 +0800

    update for index

--- src/modules/home/index/Index.vue
+++ src/modules/home/index/Index.vue
@@ -28,7 +28,7 @@
         <div class="row picture-row">
           <div class="col-md-4">
             <div class="index-picture-wrapper">
-              <img :src="imgUrl('index2.png')">
+              <img :src="imgUrl('index2.png')"/>
               <p class="index-picture-description" v-show="currentLang=='zh_hk'">
                 <router-link to="/njgx">英國牛津國際公學</router-link>
               </p>
@@ -39,7 +39,7 @@
           </div>
           <div class="col-md-4">
             <div class="index-picture-wrapper">
-              <img :src="imgUrl('index3.png')">
+              <img :src="imgUrl('index3.png')" />
               <p class="index-picture-description" v-show="currentLang=='zh_hk'">
                 <router-link to="/njkc">崇正牛津國際中學課程</router-link>
               </p>
@@ -50,7 +50,8 @@
           </div>
           <div class="col-md-4">
             <div class="index-picture-wrapper">
-              <img :src="imgUrl('index1.png')">
+              <img :src="imgUrl('index1.png')" />
+            </div>
             </div>
           </div>
           <div class="row">
@@ -63,89 +64,94 @@
               </div>
             </div>
           </div>
-        </div>
-        <div class="row picture-row">
-          <div class="col-md-4">
-            <div class="index-picture-wrapper">
-              <img :src="imgUrl('index4.png')">
-              <p class="index-picture-description" v-show="currentLang=='zh_hk'">
-                <router-link to="/rxsq">入學申请</router-link>
-              </p>
-              <p class="index-picture-description" v-show="currentLang=='en'">
-                <router-link to="/rxsq">Admission Guideline</router-link>
-              </p>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="index-picture-wrapper">
-              <img :src="imgUrl('index5.png')">
-              <p class="index-picture-description" v-show="currentLang=='zh_hk'">
-                <router-link to="/njxy">崇正牛津校園</router-link>
-              </p>
-              <p class="index-picture-description" v-show="currentLang=='en'">
-                <router-link to="/njxy">TTMS OIC Campus</router-link>
-              </p>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="index-picture-wrapper">
-              <img :src="imgUrl('index6.png')">
-              <p class="index-picture-description" v-show="currentLang=='zh_hk'">
-                <router-link to="/honor">光榮榜與傑出學生</router-link>
-              </p>
-              <p class="index-picture-description" v-show="currentLang=='en'">
-                <router-link to="/honor">Honorary List</router-link>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="row picture-row">
-          <div class="col-xs-12">
-            <div class="index-picture-wrapper">
-              <img :src="imgUrl('czzx-xg.png')">
-            </div>
-            </div>
-          </div>
           <div class="row picture-row">
-            <div class="col-xs-12">
+            <div class="col-md-4">
               <div class="index-picture-wrapper">
-                <img :src="imgUrl('czzx-2.png')">
-            </div>
+                <img :src="imgUrl('index4.png')" />
+                <p class="index-picture-description" v-show="currentLang=='zh_hk'">
+                  <router-link to="/rxsq">入學申请</router-link>
+                </p>
+                <p class="index-picture-description" v-show="currentLang=='en'">
+                  <router-link to="/rxsq">Admission Guideline</router-link>
+                </p>
               </div>
             </div>
-            <div class="row picture-row">
-              <div class="col-xs-12">
-                <div class="index-picture-wrapper">
-                  <img :src="imgUrl('czzx-3.png')">
-            </div>
-                </div>
+            <div class="col-md-4">
+              <div class="index-picture-wrapper">
+                <img :src="imgUrl('index5.png')">
+                <p class="index-picture-description" v-show="currentLang=='zh_hk'">
+                  <router-link to="/njxy">崇正牛津校園</router-link>
+                </p>
+                <p class="index-picture-description" v-show="currentLang=='en'">
+                  <router-link to="/njxy">TTMS OIC Campus</router-link>
+                </p>
               </div>
-              <div class="row picture-row">
-                <div class="col-xs-12">
-                  <div class="index-picture-wrapper">
-                    <img :src="imgUrl('njgx.jpg')">
             </div>
+            <div class="col-md-4">
+              <div class="index-picture-wrapper">
+                <img :src="imgUrl('index6.png')"/>
+                <p class="index-picture-description" v-show="currentLang=='zh_hk'">
+                  <router-link to="/honor">光榮榜與傑出學生</router-link>
+                </p>
+                <p class="index-picture-description" v-show="currentLang=='en'">
+                  <router-link to="/honor">Honorary List</router-link>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="row picture-row picture-row-2">
+            <div class="col-xs-12">
+              <div class="index-school-title">
+                <div class="school-logo"><img :src="imgUrl('czzx-logo.png')"/></div>
+                  <div class="school-name">
+                    {{'schoolName' | translate(translator, currentLang)}}
                   </div>
                 </div>
-                <div class="row picture-row">
-                  <div class="col-xs-12">
+              </div>
+              <div class="col-xs-7">
+                <div class="index-picture-wrapper">
+                  <img :src="imgUrl('cz_bg.png')" />
+                </div>
+                  <div class="index-picture-wrapper">
+                    <img :src="imgUrl('czzx-2.png')" />
+                  </div>
+                  </div>
+                  <div class="col-xs-5">
                     <div class="index-picture-wrapper">
-                      <img :src="imgUrl('czzx-4.jpg')">
+                      <img :src="imgUrl('gate.jpg')" />
             </div>
                     </div>
-                  </div>
-                </div>
-                <div class="container-fluid picture-container">
-                  <div class="row picture-row">
                     <div class="col-xs-12">
                       <div class="index-picture-wrapper">
-                        <img :src="imgUrl('czzx-plan.jpg')">
-            </div>
+                        <img :src="imgUrl('njgx.png')" />
+                      </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              </div>
+                    <div class="row picture-row">
+                      <div class="col-xs-12">
+                        <div class="index-school-title text-right">
+                          <div class="school-logo"><img :src="imgUrl('logo.png')"/></div>
+                            <div class="school-name">{{'njSchoolName' | translate(translator, currentLang)}}</div>
+                          </div>
+                        </div>
+                        <div class="col-xs-12">
+                          <div class="index-picture-wrapper">
+                            <img :src="imgUrl('czzx-4.jpg')" />
+                          </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="container-fluid picture-container">
+                        <div class="row picture-row">
+                          <div class="col-xs-12">
+                            <div class="index-picture-wrapper">
+                              <img :src="imgUrl('czzx-plan.jpg')"/>
+            </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
 </template>
 <script src="./index.js"></script>
 <style type="text/css">
